9a456f711b7e36ef7b191aea882cb528df9fea7d
Priyanka <priyankashah3107@gmail.com>
2024-08-03 23:27:42 +0530

    card: CaseStudies

---
+++ src/components/Case.tsx
@@ -0,0 +1,31 @@
+"use client"
+import Image from "next/image"
+import React from "react"
+interface CaseProps {
+  title: string, 
+  img: string,
+  para: string,
+ 
+}
+
+
+const Case: React.FC<CaseProps> = ({title, para,  img,}) => {
+  return (
+    <>
+    <div className='card lg:w-full case-slide text-white bg-[#191A23]   h-[310px]' 
+     >
+      <div className='flex flex-col gap-6'>
+      <p className=" w-[180px] lg:w-[286px] lg:text-lg text-base font-normal leading-normal text-white">{title}</p>
+      <div className="flex flex-row items-center gap-4">
+      <h3 className='text-[#B9FF66]'>{para}</h3>
+      <Image src={img} alt="img" width={21} height={21}/>
+      </div>
+     
+       </div>
+    </div>
+    </>
+  )
+}
+
+export default Case;
+
--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -1,62 +1,26 @@
-import Image from 'next/image'
 import React from 'react'
+import Case from './Case'
 
 const head = "Case Studies"
 const para = "Explore Real-Life Examples of Our Proven Digital Marketing Success through Our Case Studies"
 
-const data = [
-  {id: 1, info: "For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.",
-  learn: "Learn more",
-  icon: "/icon.svg"
-  },
-  {id: 2, info: "For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.",
-    learn: "Learn more",
-    icon: "/icon.svg"
-    },
-    {id: 3, info: "For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.",
-      learn: "Learn more",
-      icon: "/icon.svg"
-      },
-      {id: 4, info: "For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic.",
-        learn: "Learn more",
-        icon: "/icon.svg"
-        },
-        {id: 5, info: "For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales.",
-          learn: "Learn more",
-          icon: "/icon.svg"
-          },
-          {id: 6, info: "For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales.",
-            learn: "Learn more",
-            icon: "/icon.svg"
-          }
-]
-
 const CaseStudies = () => {
   return (
     <>
-      <div className='p-4 pl-6  lg:pl-20 lg:p-10  flex flex-col items-center lg:flex-row gap-6 lg:gap-10 mt-4 lg:mt-24 '>
+      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-col items-center gap-6 lg:gap-10 mt-4 lg:mt-24'>
         <h2 className='services md:w-[265px] md:h-[52px]'>{head}</h2>
-        <p className='spara lg:text-lg  lg:text-start lg:text-[18px] lg:w-[580px]'>{para}</p>
+        <p className='spara lg:text-lg lg:text-start lg:text-[18px] lg:w-[580px]'>{para}</p>
       </div>
         
-        <div className='flex flex-row gap-14 p-4 pl-6  lg:pl-20 lg:p-10 logo-contains'>
-          {data.map((val, idx) => (
-            <div key={idx} className="" >
-              <div className='lg:w-full rounded-lg bg-[#191A23] text-[#ffffff] p-4 logo-slide '>
-              <p>{val.info}</p>
-              <div className='flex flex-row items-center '>
-              <p>{val.learn}</p>
-               <Image src={val.icon} alt='icon' width={21} height={20} />
-              </div>
-              </div>
-              
-            </div>
-          ))}
-        </div>
-        
+      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-row gap-8 w-[350px] md:w-[750px] lg:w-full overflow-hidden cursor-pointer'>
 
+        <Case title={"For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales."} para={"Learn more"} img={"/icon.svg"} />
+        <Case title={"For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic."} para={"Learn more"} img={"/icon.svg"} />
+        <Case title={"For a national retail chain, we created a social media marketing campaign that increased followers by 25% and generated a 20% increase in online sales."} para={"Learn more"} img={"/icon.svg"} />
+        <Case title={"For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic."} para={"Learn more"} img={"/icon.svg"} />
+      </div>
     </>
   )
 }
 
-export default CaseStudies
+export default CaseStudies;
