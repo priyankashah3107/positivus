a9e2ae5932939dda2e917d238e6f3a9c12275b8d
Priyanka <priyankashah3107@gmail.com>
2024-08-04 22:21:52 +0530

    feat: testinomialCard

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,21 +1,36 @@
+"use client"
+import Testi from './Testi'
+import { ArrowLeftCircle, ArrowRightCircle, Star } from 'lucide-react'
 import React from 'react'
-
+import Image from 'next/image'
 const head = "Testimonials"
 const para = "Hear from Our Satisfied Clients: Read Our Testimonials to Learn More about Our Digital Marketing Services"
+
+const stars = [{ id: 1, star: "/greenstar.svg" }];
+
+for (let i = 2; i <= 5; i++) {
+  stars.push({ id: i, star: "/whitestar.svg" });
+}
+
 
 const Testimonials = () => {
   return (
    <>
    <div className="p-4 pl-6 lg:pl-20 lg:p-10">
-   <div className=' flex flex-col lg:flex-row items-center gap-6 lg:gap-10 mt-32 lg:mt-24'>
+   <div className=' flex flex-col lg:flex-row items-center gap-6 lg:gap-10 mt-32 lg:mt-24 autoShow'>
         <h2 className='services test  md:w-[265px] md:h-[52px]'>{head}</h2>
         <p className='spara lg:text-lg lg:text-start lg:text-[18px] lg:w-[580px]'>{para}</p>
       </div>
 
-      <div className="testinomial lg:w-full mt-10 ">
-
+      <div className="testinomial flex flex-row overflow-hidden  lg:w-full mt-10 ">
+        <Testi para={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aspernatur velit culpa veritatis expedita excepturi ut nulla in eius voluptate?"} name={"Jhon Smith"} desc={"Marketing Director at XYZ Corp"} />
+        <Testi para={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aspernatur velit culpa veritatis expedita excepturi ut nulla in eius voluptate?"} name={"Jhon Smith"} desc={"Marketing Director at XYZ Corp"} />
+        <Testi para={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aspernatur velit culpa veritatis expedita excepturi ut nulla in eius voluptate?"} name={"Jhon Smith"} desc={"Marketing Director at XYZ Corp"} />
+        <Testi para={"Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat aspernatur velit culpa veritatis expedita excepturi ut nulla in eius voluptate?"} name={"Jhon Smith"} desc={"Marketing Director at XYZ Corp"} />
       </div>
    </div>
+
+  
    </>
   )
 }
