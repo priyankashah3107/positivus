8cbd47859366a72403d0b5cbf89f2187be67c2e3
Priyanka <priyankashah3107@gmail.com>
2024-08-04 11:00:51 +0530

    fix: CaseCard for lg

--- src/components/CaseStudies.tsx
+++ src/components/CaseStudies.tsx
@@ -7,12 +7,12 @@ const para = "Explore Real-Life Examples of Our Proven Digital Marketing Success
 const CaseStudies = () => {
   return (
     <>
-      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-col items-center gap-6 lg:gap-10 mt-4 lg:mt-24'>
+      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-col lg:flex-row items-center gap-6 lg:gap-10 mt-4 lg:mt-24 '>
         <h2 className='services md:w-[265px] md:h-[52px]'>{head}</h2>
         <p className='spara lg:text-lg lg:text-start lg:text-[18px] lg:w-[580px]'>{para}</p>
       </div>
         
-      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-row gap-8 w-[350px] md:w-[750px] lg:w-full overflow-hidden cursor-pointer'>
+      <div className='p-4 pl-6 lg:pl-20 lg:p-10 flex flex-row gap-8 lg:gap-0 w-[350px] md:w-[750px] lg:w-full overflow-hidden cursor-pointer'>
 
         <Case title={"For a local restaurant, we implemented a targeted PPC campaign that resulted in a 50% increase in website traffic and a 25% increase in sales."} para={"Learn more"} img={"/icon.svg"} />
         <Case title={"For a B2B software company, we developed an SEO strategy that resulted in a first page ranking for key keywords and a 200% increase in organic traffic."} para={"Learn more"} img={"/icon.svg"} />
