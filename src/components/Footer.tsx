e41686c7706b77fae0ebcf8cffbf3b77cc6fa655
Priyanka <shahpriyanka9971@gmail.com>
2024-08-08 07:15:40 -0700

    fix: Footer Responvieness

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -32,14 +32,14 @@ const Footer = () => {
     <div className="p-4 pl-8 lg:pl-20 lg:p-10  text-white">
       <div className="flex flex-col w-[400px] md:w-full gap-8 bg-gray-900 p-20 rounded-tr-2xl rounded-tl-2xl">
         
-        <div className="flex flex-col md:flex-row justify-between items-center gap-6 md:gap-10">
+        <div className="flex flex-col lg:flex-row justify-between items-center gap-6 md:gap-10">
           <div>
             <Image src="/logo.png" alt="logo" width={100} height={23} className="cursor-pointer lg:w-[180px] lg:h-[29px]" />
           </div>
           
-          <div className="flex flex-col md:flex-row justify-center gap-4 md:gap-10 lg:ml-24">
+          <div className="flex flex-col lg:flex-row justify-center gap-4 md:gap-10 lg:ml-24">
             {fooNav.map((val) => (
-              <p key={val.id} className="cursor-pointer fooCon md:text-[20px] items-center text-center md:mb-0 md:underline">{val.info}</p>
+              <p key={val.id} className="cursor-pointer fooCon md:text-[20px] items-center text-center lg:mb-0 md:underline">{val.info}</p>
             ))}
           </div>
           
@@ -50,7 +50,7 @@ const Footer = () => {
           </div>
         </div>
         
-        <div className="flex flex-col md:flex-row justify-between gap-8 md:gap-10 ">
+        <div className="flex flex-col   justify-center items-center text-center lg:flex-row lg:justify-between  gap-8 md:gap-10 lg:mt-10 ">
           <div className="flex flex-col gap-2 text-center items-center md:text-start md:items-start">
             <h3 className="bg-[#B9FF66] rounded-md font-medium text-black w-max px-2 py-1">Contact us:</h3>
             {emailInfo.map((val) => (
@@ -58,9 +58,9 @@ const Footer = () => {
             ))}
           </div>
           
-          <div className="flex flex-col md:flex-row items-center gap-4 bg-[#292A32] p-10 rounded-xl">
-            <input type="email" placeholder="Email" className="p-2 border border-white rounded-lg w-full md:w-auto" />
-            <button className="bg-[#B9FF66] p-2 text-black rounded-lg font-semibold w-full md:w-auto">Subscribe to news</button>
+          <div className="flex flex-col lg:flex-row items-center lg:items-center  gap-4 bg-[#292A32] lg:pl-10 lg:pr-10 w-[380px] h-[200px] md:w-1/2 lg:w-1/3  rounded-xl">
+            <input type="email" placeholder="Email" className="p-2 border border-white mt-6 lg:mt-0 lg:ml-0 rounded-lg w-[330px] h-[68px] md:w-[249px]" />
+            <button className="bg-[#B9FF66] w-[330px] h-[68px] md:w-[249px] p-2 text-black rounded-lg font-semibold">Subscribe to news</button>
           </div>
         </div>
         
